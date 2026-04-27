67e87a78ba51c4c11212ab9266fce3829474eafc
aryabickij <aryabickij@lmteam.by>
2026-04-28 01:13:03 +0300

    Дополнение к главному экрану

--- PlaylistMakerSwift/MainMenu.swift
+++ PlaylistMakerSwift/MainMenu.swift
@@ -8,57 +8,64 @@ struct MainMenu: View {
                 .font(.system(size: 22, weight: Font.Weight.medium))
                 .padding(EdgeInsets(top: 0, leading: 16, bottom: 40, trailing: 0))
                 
-            
             ButtonMainMenu(
                 label: "Поиск",
                 image: ImageResource.mainMenuIcSearch
-            )
-                .padding(.horizontal, 16)
-                .layoutPriority(1)
+            ) {
+                
+            }
             
             Spacer(minLength: 16)
             
             ButtonMainMenu(
                 label: "Медиатека",
                 image: ImageResource.mainMenuIcMedia
-            )
-                .padding(.horizontal, 16)
-                .layoutPriority(1)
+            ) {
+                
+            }
             
             Spacer(minLength: 16)
             
             ButtonMainMenu(
                 label: "Настройки",
                 image: ImageResource.mainMenuIcSettings
-            )
-            .padding(.horizontal, 16)
-            .layoutPriority(1)
-            
-            
+            ) {
+                
+            }
         }
         .background(Color.mainMenuBackground)
     }
 }
 
-private func ButtonMainMenu (
-    label: String,
-    image: ImageResource
-) -> some View {
-    Button(
-        action: {
-        
-        }
-    ) {
-        HStack(spacing: 8) {
-            Image(image)
-            Text(label)
-                .foregroundColor(Color.mainMenuColorButtonText)
-                .font(.system(size: 22, weight: Font.Weight.medium))
-        }
+private struct ButtonMainMenu: View {
+    private let label: String
+    private let image: ImageResource
+    private let action: () -> Void
+    
+    init(label: String, image: ImageResource, action: @escaping () -> Void) {
+        self.label = label
+        self.image = image
+        self.action = action
     }
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background(Color.white)
-    .cornerRadius(16)
+    
+    fileprivate var body: some View {
+        Button(
+            action: {
+                action()
+            }
+        ) {
+            HStack(spacing: 8) {
+                Image(image)
+                Text(label)
+                    .foregroundColor(Color.mainMenuColorButtonText)
+                    .font(.system(size: 22, weight: Font.Weight.medium))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        .cornerRadius(16)
+        .padding(.horizontal, 16)
+    }
 }
 
 #Preview {
--- PlaylistMakerSwift/PlaylistMakerSwiftApp.swift
+++ PlaylistMakerSwift/PlaylistMakerSwiftApp.swift
@@ -1,17 +1,10 @@
-//
-//  PlaylistMakerSwiftApp.swift
-//  PlaylistMakerSwift
-//
-//  Created by Андрей on 27.04.2026.
-//
-
 import SwiftUI
 
 @main
 struct PlaylistMakerSwiftApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainMenu()
         }
     }
 }
