ebcf803938376fc17778c069e0a6fca083a0ed8e
aryabickij <aryabickij@lmteam.by>
2026-04-27 22:17:53 +0300

    Initial Commit

---
+++ PlaylistMakerSwift/PlaylistMakerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistMakerSwiftApp.swift
+//  PlaylistMakerSwift
+//
+//  Created by Андрей on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistMakerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
